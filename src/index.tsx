ad0eb27f1f011dcbad7294c9647e262402808159
Nojus0 <thealextgm@gmail.com>
2022-04-02 17:28:23 +0100

    Add Audio Preload

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import Home from "./pages";
+import { preload } from "./utils/Audio";
+
+preload();
 
 render(() => <Home />, document.getElementById("root") as HTMLElement);
--- src/utils/Sounds.ts
+++ src/utils/Sounds.ts
@@ -1,5 +1,5 @@
-import { PlaySync } from "./Audio";
+import { play } from "./Audio";
 
 export function playErrorSound() {
-  PlaySync("/sound/mainmenu_press_quit_02.wav");
+  play("/sound/mainmenu_press_quit_02.wav");
 }
