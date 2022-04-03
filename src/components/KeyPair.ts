7eff433c28b1286ac735d97cb56d629b3b2d1e82
Nojus0 <thealextgm@gmail.com>
2022-04-03 18:14:19 +0100

    Add versioning, Fix IV size, Increase iterations, Add Salt and Muting

--- src/components/KeyPair.ts
+++ src/components/KeyPair.ts
@@ -3,32 +3,37 @@ import { DEBUG_ALL_LOADED } from "../utils/debug";
 import { loadJsonFile, writeFile } from "./filesystem-abstract";
 import { ILoaded } from "./utils";
 
-export const AES_IV_BYTES = 128;
-export const AES_KEY_BITS = 256;
+export const AES_IV_BYTES = 32;
+export const AES_KEY_BITS = AES_IV_BYTES * 8;
+export const VERSION = 1;
 
 export interface IKeyPairSerialized {
   key: string;
   iv: string;
+  version: number;
 }
 
 export interface IKeyPair {
   key: CryptoKey;
   iv: Uint8Array;
+  version: number;
 }
 
 export type KeyPairStore = IKeyPair & ILoaded;
 
-export const defaultKeyPairStore = () => ({
-  iv: null as any,
-  key: null as any,
-  loaded: DEBUG_ALL_LOADED,
-});
+export const defaultKeyPairStore = () =>
+  ({
+    loaded: DEBUG_ALL_LOADED,
+  } as KeyPairStore);
 
 export const createNewKeypair = async () => {
   const CRYPTO_KEY = await crypto.subtle.generateKey(
     {
       name: "AES-GCM",
       length: AES_KEY_BITS,
+      hash: "SHA-512",
+      salt: crypto.getRandomValues(new Uint8Array(AES_IV_BYTES)),
+      iterations: 10000000,
     },
     true,
     ["encrypt", "decrypt"]
@@ -36,6 +41,7 @@ export const createNewKeypair = async () => {
 
   const a: IKeyPair = {
     key: CRYPTO_KEY,
+    version: VERSION,
     iv: crypto.getRandomValues(new Uint8Array(AES_IV_BYTES)),
   };
 
@@ -47,11 +53,13 @@ export async function loadKeyPair() {
 
   const PAIR: IKeyPair = {
     iv: base64.toByteArray(a.iv),
+    version: a.version,
     key: await crypto.subtle.importKey(
       "raw",
       base64.toByteArray(a.key),
       {
         name: "AES-GCM",
+        hash: "SHA-512",
         length: AES_KEY_BITS,
       },
       true,
@@ -70,7 +78,8 @@ export async function exportKeyPair(keypair: IKeyPair) {
   const SERIALIZED_KEYPAIR: IKeyPairSerialized = {
     key: base64.fromByteArray(KEY_BUF),
     iv: base64.fromByteArray(keypair.iv),
+    version: keypair.version,
   };
-  
+
   await writeFile(JSON.stringify(SERIALIZED_KEYPAIR, null, 2), ".json");
 }
