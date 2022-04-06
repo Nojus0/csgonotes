75f2757afc9e020e68c1c13ebdc929f48800b0f3
Nojus0 <thealextgm@gmail.com>
2022-04-06 20:12:00 +0100

    Rollback

--- src/utils/Audio.ts
+++ src/utils/Audio.ts
@@ -1,16 +1,21 @@
 import { userInteracted } from "./ChromeAudio";
 
-// const audioLoadList = [
-//   "/sound/generic_press_01.wav",
-//   "/sound/mainmenu_press_quit_02.wav",
-//   "/sound/itemtile_rollover_09.wav",
-// ];
+const audioLoadList = [
+  "/sound/generic_press_01.wav",
+  "/sound/mainmenu_press_quit_02.wav",
+  "/sound/itemtile_rollover_09.wav",
+];
 
-// export async function preloadPrimitiveAudio() {
-//   for (const url of audioLoadList) {
-//     preloadAudio(url);
-//   }
-// }
+export async function preloadPrimitiveAudio() {
+  for (const url of audioLoadList) {
+    preloadAudio(url);
+  }
+}
+
+export async function preloadAudio(src: string) {
+  var audio = new Audio();
+  audio.src = src;
+}
 
 export function play(src: string, vol: number = 1.0) {
   if (!userInteracted()) return;
@@ -21,8 +26,3 @@ export function play(src: string, vol: number = 1.0) {
   audio.volume = vol;
   audio.play();
 }
-
-// export async function preloadAudio(src: string) {
-//   var audio = new Audio();
-//   audio.src = src;
-// }
