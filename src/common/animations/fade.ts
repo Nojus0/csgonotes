8985cfd2a9843d95abc3dfa8a08c34aad89f7cb3
Nojus0 <thealextgm@gmail.com>
2022-04-10 23:14:35 +0100

    Improve First paint performance

---
+++ src/common/animations/fade.ts
@@ -0,0 +1,32 @@
+import { TransitionProps } from "./interfaces";
+
+export function getFadeAnimation(duration: number): TransitionProps {
+  return {
+    onEnter: (e, done) => {
+      e.animate(
+        [
+          {
+            opacity: 0,
+          },
+          {
+            opacity: 1,
+          },
+        ],
+        { duration, easing: "ease-in-out" }
+      ).finished.then(done);
+    },
+    onExit: (e, done) => {
+      e.animate(
+        [
+          {
+            opacity: 1,
+          },
+          {
+            opacity: 0,
+          },
+        ],
+        { duration, easing: "ease-in-out" }
+      ).finished.then(done);
+    },
+  };
+}
---
+++ src/common/animations/interfaces.ts
@@ -0,0 +1,19 @@
+import { JSX } from "solid-js";
+export type TransitionProps = {
+  name?: string;
+  enterActiveClass?: string;
+  enterClass?: string;
+  enterToClass?: string;
+  exitActiveClass?: string;
+  exitClass?: string;
+  exitToClass?: string;
+  onBeforeEnter?: (el: Element) => void;
+  onEnter?: (el: Element, done: () => void) => void;
+  onAfterEnter?: (el: Element) => void;
+  onBeforeExit?: (el: Element) => void;
+  onExit?: (el: Element, done: () => void) => void;
+  onAfterExit?: (el: Element) => void;
+  children?: JSX.Element;
+  appear?: boolean;
+  mode?: "inout" | "outin";
+};
