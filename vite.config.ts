f6bba8e8b23523cf6cea56335ab2feac5e8b002a
Nojus0 <thealextgm@gmail.com>
2022-06-10 20:03:22 +0100

    Add Additional Error handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import pathsPlugin from "vite-tsconfig-paths";
+
+// ! No Vendor js file ?
+
 export default defineConfig({
   plugins: [pathsPlugin(), solidPlugin()],
   build: {
