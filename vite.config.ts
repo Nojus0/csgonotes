ee859e5d4ed4f341a88db74594485b30c1a6af1e
Nojus0 <thealextgm@gmail.com>
2022-06-13 19:12:18 +0100

    'vendor.js' Not being Output Fix & Netlify Redirects

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import pathsPlugin from "vite-tsconfig-paths";
 
 // ! No Vendor js file ?
 
 export default defineConfig({
-  plugins: [pathsPlugin(), solidPlugin()],
+  plugins: [splitVendorChunkPlugin(), pathsPlugin(), solidPlugin()],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
