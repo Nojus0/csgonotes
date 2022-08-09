3887f7ea3ef68d7a7b5a8dd6ff45c3cc2ae1c8f6
Nojus0 <thealextgm@gmail.com>
2022-08-09 21:07:57 +0100

    Test Pnpm & Netlify Building

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,9 @@ import { defineConfig, splitVendorChunkPlugin } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import pathsPlugin from "vite-tsconfig-paths";
 
-// ! No Vendor js file ?
-
 export default defineConfig({
   plugins: [pathsPlugin(), solidPlugin(), splitVendorChunkPlugin()],
   build: {
     target: "esnext",
-    polyfillDynamicImport: false,
   },
 });
