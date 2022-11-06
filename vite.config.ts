3ef4b7815f3f258682f2ea3baf93dad8677336a6
Nojus0 <thealextgm@gmail.com>
2022-11-06 22:56:46 +0000

    Change from standalone to fullscreen mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,43 @@
-import {defineConfig, splitVendorChunkPlugin} from "vite";
-import solidPlugin from "vite-plugin-solid";
-import pathsPlugin from "vite-tsconfig-paths";
-import {serviceWorker} from "./src/lib/vite-service-worker";
+import { defineConfig, splitVendorChunkPlugin } from "vite"
+import solidPlugin from "vite-plugin-solid"
+import pathsPlugin from "vite-tsconfig-paths"
+import { serviceWorker } from "./src/lib/vite-service-worker"
 
 export default defineConfig({
-    plugins: [
-        pathsPlugin(),
-        solidPlugin(),
-        splitVendorChunkPlugin(),
-        serviceWorker({
-            manifest: {
-                short_name: 'Notes',
-                name: 'CS:GO Notes',
-                start_url: '/',
-                scope: '/',
-                theme_color: '#4caf50',
-                background_color: '#4caf50',
-                display: 'standalone',
-                orientation: 'portrait',
-                description: "A CS:GO Themed Notes Web App with AES256 Encryption.",
-                icons: [
-                    {
-                        src: '/static/icons/icon.svg',
-                        type: 'image/svg+xml',
-                        sizes: 'any',
-                        purpose: 'any',
-                    },
-                    {
-                        src: "/static/icons/icon.png",
-                        type: "image/png",
-                        sizes: "128x128",
-                        purpose: "any"
-                    }
-                ],
-                screenshots: []
-            }
-        })
-    ],
-    build: {
-        target: "esnext",
-    },
-});
+  plugins: [
+    pathsPlugin(),
+    solidPlugin(),
+    splitVendorChunkPlugin(),
+    serviceWorker({
+      manifest: {
+        short_name: "Notes",
+        name: "CS:GO Notes",
+        start_url: "/",
+        scope: "/",
+        theme_color: "#4caf50",
+        background_color: "#4caf50",
+        display: "fullscreen",
+        orientation: "portrait",
+        description: "A CS:GO Themed Notes Web App with AES256 Encryption.",
+        icons: [
+          {
+            src: "/static/icons/icon.svg",
+            type: "image/svg+xml",
+            sizes: "any",
+            purpose: "any",
+          },
+          {
+            src: "/static/icons/icon.png",
+            type: "image/png",
+            sizes: "128x128",
+            purpose: "any",
+          },
+        ],
+        screenshots: [],
+      },
+    }),
+  ],
+  build: {
+    target: "esnext",
+  },
+})
